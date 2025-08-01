a394c3dd5e8b80139f23d9579d67bb37c334b315
angrykim18 <khbkhs05@gmail.com>
2025-08-01 13:46:57 +0900

    Delete src directory

--- src/main/java/com/newez/backend/repository/UserGroupRepository.java
+++
@@ -1,7 +0,0 @@
-package com.newez.backend.repository;
-
-import com.newez.backend.domain.UserGroup;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserGroupRepository extends JpaRepository<UserGroup, Long> {
-}
