5d7484becce80ba7e5353e32f6ee6fc4230a1580
angrykim18 <khbkhs05@gmail.com>
2025-08-04 17:53:06 +0900

    Delete src directory

--- src/main/java/com/newez/backend/repository/NoticeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.newez.backend.repository;
-
-import com.newez.backend.domain.Notice;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NoticeRepository extends JpaRepository<Notice, Long> {
-}
