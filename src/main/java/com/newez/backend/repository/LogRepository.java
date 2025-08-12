962a5fb112528b33062c346ecd008aed03a18410
angrykim18 <khbkhs05@gmail.com>
2025-08-12 11:33:03 +0900

    Delete src directory

--- src/main/java/com/newez/backend/repository/LogRepository.java
+++
@@ -1,7 +0,0 @@
-package com.newez.backend.repository;
-
-import com.newez.backend.domain.Log;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LogRepository extends JpaRepository<Log, Long> {
-}
