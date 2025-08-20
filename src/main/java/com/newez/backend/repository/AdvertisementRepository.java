119b4cbfda861a767511fbb3dc1ceaee64aa1ea1
angrykim18 <khbkhs05@gmail.com>
2025-08-20 21:34:51 +0900

    Delete src directory

--- src/main/java/com/newez/backend/repository/AdvertisementRepository.java
+++
@@ -1,7 +0,0 @@
-package com.newez.backend.repository;
-
-import com.newez.backend.domain.Advertisement;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AdvertisementRepository extends JpaRepository<Advertisement, Long> {
-}
